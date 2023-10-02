a3b059eee9bce2b613514003b0021c11117a60d3
vishz45 <zagdevishu@gmail.com>
2023-10-02 14:43:51 +0530

    vishal

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var request = require('supertest');
+
+var app = require('../index.js');describe('GET /', function() {
+
+ it('respond with hello world', function(done) { //navigate to root and check the the response is "vishal"
+
+ request(app).get('/').expect('hello all of you', done);
+
+ });
+
+});
